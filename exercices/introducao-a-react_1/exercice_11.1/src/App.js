f7e2d4b8416e28698ac843abfd1a4b8ba772b601
Camila Arruda <camilaam1802@gmail.com>
2021-04-26 19:07:06 -0300

    Resoluçao exercicio 1 e 2

--- exercices/introducao-a-react_1/exercice_11.1/src/App.js
+++ exercices/introducao-a-react_1/exercice_11.1/src/App.js
@@ -1,8 +1,18 @@
 /* import logo from './logo.svg'; */
 import './App.css';
 
+
+const arrayCompromissos = ['Acordar', 'Estuadar de manhã', 'Almoçar', 'Arrumar cozinha', 'Estudar de tarde','Jantar', 'Dormir'];
+
+const ListaDeCompromissos = (array) => {
+  return array.map((compromiso) => <li>{compromiso}</li>);
+}
+
 function App() {
-  return ();
+  return (
+    ListaDeCompromissos(arrayCompromissos)
+  
+  );
 }
 
 export default App;
